7ed6f0cf5cd139b71a1c8fe3b8966394155763f2
kyobrin <Zullisun@gmail.com>
2022-12-27 17:08:52 -0300

    Update jokenpo.py
    adicionais

--- jokenpo.py
+++ jokenpo.py
@@ -1,20 +1,18 @@
 import random
-
 print ('========JOKENPO GAME=======')
-
 npc = ['PEDRA', 'PAPEL', 'TESOURA']
-
+print ('VOCÊ OUSA ME DESAFIAR PLEBE?')
 print ('''ESCOLHA UM DESSES 3
 [1] PEDRA
 [2] PAPEL
 [3] TESOURA ''')
 
-
-print ('VOCÊ OUSA ME DESAFIAR PLEBE?')
 escolha = int(input('ESCOLHA UM DESSES E VEJA SE GANHARÁ DE MIM MERO PLEBEU: '))
+
 
 match escolha:
     case 1:
+        print ('-='*20)
         print ('Você escolheu PEDRA')
         ia =  random.choice(npc)
         print (f'EU ESCOLHO {ia} HAHAHA' )
@@ -24,26 +22,30 @@ match escolha:
             print ('VOCÊ PERDEU PLEBE HAHAHA!!')
         elif ia == 'TESOURA':
             print ('DROGA FUI VENCIDO?? NÃOOO!!')
+        print ('-='*20)
     case 2:
+        print ('-='*20)
         print ('Você escolheu PAPEL')
         ia =  random.choice(npc)
         print (f'EU ESCOLHO {ia} HAHAHA' )
         if ia == 'PAPEL':
             print ('DROGA, FOI UM EMPATE!!')
         elif ia == 'TESOURA':
-            print ('VOCÊ PERDEU  PLEBE HAHAHA!!')
+            print ('VOCÊ PERDEU PLEBE HAHAHA!!')
         elif ia == 'PEDRA':
             print ('DROGA FUI VENCIDO?? NÃOOO!!')
+        print ('-='*20)
     case 3:
+        print ('-='*20)
         print ('Você escolheu TESOURA')
         ia =  random.choice(npc)
         print (f'EU ESCOLHO {ia} HAHAHA' )
         if ia == 'TESOURA':
             print ('DROGA, FOI UM EMPATE!!')
         elif ia == 'PEDRA':
-            print ('VOCÊ PERDEU  PLEBE HAHAHA!!')
+            print ('VOCÊ PERDEU PLEBE HAHAHA!!')
         elif ia == 'PAPEL':
             print ('DROGA FUI VENCIDO?? NÃOOO!!')
-
+        print ('-='*20)
 
 
